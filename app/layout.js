6ce2fbe29768437c0b28a046e6b7e97b0ebd2823
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-31 17:57:31 +0530

    Add manifest.json

--- app/layout.js
+++ app/layout.js
@@ -3,13 +3,15 @@ import fonts from "./fonts";
 
 export const metadata = {
   title: "Chatsvilla: Instant Conversations at Your Fingertips",
-  description: "Chatsvilla: Embrace the power of real-time communication with our cutting-edge chat application. Connect with friends, family, and colleagues effortlessly, share ideas, and stay in sync like never before. Experience smooth and seamless conversations with our user-friendly interface and enjoy a secure platform that prioritizes your privacy. Say hello to the future of chat applications with Chatsvilla",
+  description:
+    "Chatsvilla: Embrace the power of real-time communication with our cutting-edge chat application. Connect with friends, family, and colleagues effortlessly, share ideas, and stay in sync like never before. Experience smooth and seamless conversations with our user-friendly interface and enjoy a secure platform that prioritizes your privacy. Say hello to the future of chat applications with Chatsvilla",
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body  className={fonts}>{children}</body>
+      <body className={fonts}>{children}</body>
     </html>
   );
 }
