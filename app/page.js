216b02146bb848a8c3da42d260a0b0351af70307
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-08-01 22:42:02 +0530

    Update index page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
+import Chat from "@/components/Chat";
 import ChatList from "@/components/ChatList";
 import React from "react";
 
 export default function Home() {
   return (
-    <div className="w-full">
+    <div className="flex w-full">
       <ChatList />
+      <Chat />
     </div>
   );
 }
