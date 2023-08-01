c7062d71d622dbf7115d8e346f062446c429247a
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-08-01 19:36:04 +0530

    Fix custom 404 page

--- app/not-found.js
+++ app/not-found.js
@@ -6,7 +6,7 @@ export const metadata = {
 
 export default function Custom404() {
   return (
-    <section className="bg-white dark:bg-black text-xl text-black dark:text-white font-firasans flex items-center justify-center h-[100vh]">
+    <section className="bg-white w-full dark:bg-black text-xl text-black dark:text-white font-firasans flex items-center justify-center h-[100vh]">
       <div className="flex items-center gap-5">
         <h1 className="font-signika text-2xl">404</h1>
         <div className="h-10 w-px bg-gray-400"></div>
