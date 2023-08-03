0bf5bcfba1d96c2d7dd9a6c43a79b7e5de46392e
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-08-03 14:54:46 +0530

    Fix ChatBubble component

--- components/ChatBubble.js
+++ components/ChatBubble.js
@@ -14,11 +14,11 @@ export default function ChatBubble({ message, timestamp, profilePic, incoming })
           src={profilePic}
           width={44}
           height={44}
-          className="w-11 h-11 z-10 p-2 bg-[#f5f3f3] dark:bg-[#2B2B2B] rounded-full"
+          className="w-11 h-11 z-10 p-1.5 bg-[#f5f3f3] dark:bg-[#2B2B2B] rounded-full"
         />
         <div className="relative bg-[#5A4DE6] dark:bg-[#161616] w-fit px-1.5 py-1 rounded-md">
-          <p className="text-sm">{message}</p>
-          <p className="text-[10px] text-right mt-px ml-4">{timestamp}</p>
+          <p className="text-sm mx-1">{message}</p>
+          <p className="text-[10px] text-right mt-px ml-4 mx-1">{timestamp}</p>
           <div
             className={`absolute ${
               incoming ? "-left-3" : "-right-3"
