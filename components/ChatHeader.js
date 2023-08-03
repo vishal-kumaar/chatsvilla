345a06831cb6c6cbab5482e203af19cf39d17885
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-08-03 20:06:47 +0530

    Add Dropdown component in ChatHeader component

--- components/ChatHeader.js
+++ components/ChatHeader.js
@@ -3,9 +3,25 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import Dropdown from "./Dropdown";
 
 export default function ChatHeader() {
   const [dropdown, setDropdown] = useState(false);
+  const dropdownOptions = [
+    {
+      name: "View Profile",
+      icon: "/icons/profile.svg",
+      link: "/profile/1",
+    },
+    {
+      name: "Delete Chat",
+      icon: "/icons/trash.svg",
+    },
+    {
+      name: "Block User",
+      icon: "/icons/block.svg",
+    },
+  ];
 
   return (
     <header className="z-50 flex justify-between items-center sticky top-0 right-0 px-7 py-2 shadow-2xl bg-[#5a4de6] dark:bg-black text-white">
@@ -40,43 +56,7 @@ export default function ChatHeader() {
           className="w-5 h-5 invert cursor-pointer"
           onClick={() => setDropdown(!dropdown)}
         />
-        {dropdown && (
-          <div
-            className={`font-firasans outline-none text-base absolute right-3 top-6 bg-white dark:bg-[#161616] text-black dark:text-white w-40 rounded-lg shadow-2xl overflow-hidden`}>
-            <Link
-              href="/profile/1"
-              className="flex items-center gap-2 px-4 py-2 hover:bg-[#DFDBDB] dark:hover:bg-[#494848] w-full">
-              <Image
-                alt=""
-                src="/icons/profile.svg"
-                height={16}
-                width={16}
-                className="w-4 h-4 invert-0 dark:invert"
-              />
-              <p>View Profile</p>
-            </Link>
-            <button className="flex items-center gap-2 px-4 py-2 hover:bg-[#DFDBDB] dark:hover:bg-[#494848] w-full">
-              <Image
-                alt=""
-                src="/icons/trash.svg"
-                height={16}
-                width={16}
-                className="w-4 h-4 invert-0 dark:invert"
-              />
-              <p>Delete Chat</p>
-            </button>
-            <button className="flex items-center gap-2 px-4 py-2 hover:bg-[#DFDBDB] dark:hover:bg-[#494848] w-full">
-              <Image
-                alt=""
-                src="/icons/block.svg"
-                height={16}
-                width={16}
-                className="w-4 h-4 invert-0 dark:invert"
-              />
-              <p>Block User</p>
-            </button>
-          </div>
-        )}
+        {dropdown && <Dropdown options={dropdownOptions} />}
       </div>
     </header>
   );
