f6f4038a114729ba91913c5038df78c8b68ef828
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-08-11 22:20:18 +0530

    Fix dropdown button text issue

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -1,46 +1,28 @@
+"use client";
+
 import React from "react";
-import Link from "next/link";
 import Image from "next/image";
 
 export default function Dropdown({ options }) {
   return (
     <div
-      className={`font-firasans outline-none text-base absolute right-3 top-6 bg-white dark:bg-[#161616] text-black dark:text-white w-40 rounded-lg shadow-2xl overflow-hidden`}>
+      className={`font-firasans outline-none text-base absolute right-3 top-6 bg-white dark:bg-[#161616] text-black dark:text-white w-fit rounded-lg shadow-2xl overflow-hidden`}>
       {options.map((option, index) => (
-        <>
-          {option.link ? (
-            <Link
-              key={index}
-              href={option.link}
-              className="flex items-center gap-2 px-4 py-2 hover:bg-[#DFDBDB] dark:hover:bg-[#494848] w-full">
-              {option.icon && (
-                <Image
-                  alt=""
-                  src={option.icon}
-                  height={16}
-                  width={16}
-                  className="w-4 h-4 invert-0 dark:invert"
-                />
-              )}
-              <p>{option.name}</p>
-            </Link>
-          ) : (
-            <button
-              key={index}
-              className="flex items-center gap-2 px-4 py-2 hover:bg-[#DFDBDB] dark:hover:bg-[#494848] w-full">
-              {option.icon && (
-                <Image
-                  alt=""
-                  src={option.icon}
-                  height={16}
-                  width={16}
-                  className="w-4 h-4 invert-0 dark:invert"
-                />
-              )}
-              <p>{option.name}</p>
-            </button>
+        <button
+          key={index}
+          className="flex items-center gap-2 pl-4 pr-8 py-2 hover:bg-[#DFDBDB] dark:hover:bg-[#494848] w-full whitespace-nowrap"
+          onClick={option.callback}>
+          {option.icon && (
+            <Image
+              alt=""
+              src={option.icon}
+              height={16}
+              width={16}
+              className="w-4 h-4 invert-0 dark:invert"
+            />
           )}
-        </>
+          <p>{option.name}</p>
+        </button>
       ))}
     </div>
   );
