0c8b419f4734531a7175ae42194a5d29bfd837c0
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-08-14 22:35:01 +0530

    Add back button in ChatHeader for small screen

--- components/ChatHeader.js
+++ components/ChatHeader.js
@@ -4,9 +4,12 @@ import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import Dropdown from "./Dropdown";
+import { useRouter } from "next/navigation";
 
 export default function ChatHeader() {
   const [dropdown, setDropdown] = useState(false);
+  const router = useRouter();
+
   const dropdownOptions = [
     {
       name: "View Profile",
@@ -24,22 +27,32 @@ export default function ChatHeader() {
   ];
 
   return (
-    <header className="z-50 flex justify-between items-center sticky top-0 right-0 px-7 py-2 shadow-2xl bg-[#5a4de6] dark:bg-black text-white">
-      <Link
-        href="/profile/1"
-        className="flex items-center gap-4 cursor-pointer">
+    <header className="z-40 flex justify-between items-center sticky top-0 right-0 px-7 py-2 shadow-2xl bg-[#5a4de6] dark:bg-black text-white">
+      <div className="flex items-center gap-4">
         <Image
-          alt="profile-pic"
-          src="/images/user.svg"
-          height={56}
-          width={56}
-          className="w-14 h-14 rounded-full"
+          alt="back"
+          src="/icons/arrow.svg"
+          height={20}
+          width={20}
+          className="invert h-5 w-5 cursor-pointer block md:hidden"
+          onClick={() => router.back()}
         />
-        <div>
-          <h1 className="font-signika">John Cena</h1>
-          <p className="font-poppins text-xs">Online</p>
-        </div>
-      </Link>
+        <Link
+          href="/profile/1"
+          className="flex items-center gap-4 cursor-pointer">
+          <Image
+            alt="profile-pic"
+            src="/images/user.svg"
+            height={56}
+            width={56}
+            className="w-14 h-14 rounded-full"
+          />
+          <div>
+            <h1 className="font-signika">John Cena</h1>
+            <p className="font-poppins text-xs">Online</p>
+          </div>
+        </Link>
+      </div>
       <div
         className="relative"
         tabIndex={0}
