bde2523eb076f1897477c1a1aab9a1d79d9d13bf
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-08-26 19:49:46 +0530

    Add updateProfile user api

---
+++ apis/user/updateProfile.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const updateProfile = async (data, token) => {
+  try {
+    const response = await axios.patch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/user/profile/update`,
+      data,
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    return error.response?.data;
+  }
+};
+
+export default updateProfile;
