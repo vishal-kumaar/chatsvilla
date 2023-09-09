157e4a0f3e9253baa70d2dc3215df77af97f3f06
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-09-09 11:20:56 +0530

    Add read message functionality

--- components/Chat.js
+++ components/Chat.js
@@ -1,18 +1,43 @@
 "use client";
 
-import React, { useEffect } from "react";
-import { usePathname } from "next/navigation";
+import React, { useContext, useEffect } from "react";
+import { useParams, usePathname } from "next/navigation";
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
 import ChatBubble from "./ChatBubble";
 import formatDate from "@/utils/formatDate";
+import markConversationAsRead from "@/apis/conversation/markConversationAsRead";
+import SessionTokenContext from "@/contexts/sessionToken/SessionTokenContext";
+import { toast } from "react-hot-toast";
+import ThemeContext from "@/contexts/theme/ThemeContext";
 
 export default function Chat({ chat }) {
   const pathname = usePathname();
   const messages = chat?.messages;
+  const { getSessionToken } = useContext(SessionTokenContext);
+  const { isDark } = useContext(ThemeContext);
+  const { conversationId } = useParams();
+
+  const readMessage = async () => {
+    const res = await markConversationAsRead(conversationId, getSessionToken());
+    if (!res?.success) {
+      toast.error("Something went wrong!", {
+        duration: 3000,
+        position: "top-center",
+        style: {
+          borderRadius: "10px",
+          background: isDark ? "#333" : "#fff",
+          color: isDark ? "#fff" : "#000",
+        },
+      });
+    }
+  };
 
   useEffect(
     () => {
+      if (pathname !== "/") {
+        readMessage();
+      }
       if (typeof window !== "undefined") {
         window.scrollBy(0, window.innerHeight);
       }
@@ -44,7 +69,9 @@ export default function Chat({ chat }) {
         <div className="flex-grow flex flex-col justify-end gap-y-3 px-2.5 py-3 font-poppins font-medium text-white bg-[#DFDBDB] dark:bg-[#2B2B2B]">
           {messages?.map((msgArr, index1) => (
             <div key={index1}>
-              <h1 className="bg-gray-400 mx-auto w-fit font-firasans text-sm rounded-md px-1">{formatDate(msgArr[0])}</h1>
+              <h1 className="bg-gray-400 mx-auto w-fit font-firasans text-sm rounded-md px-1">
+                {formatDate(msgArr[0])}
+              </h1>
               {msgArr[1].map((message, index2) => (
                 <ChatBubble
                   key={index2}
