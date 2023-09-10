6090be5f8c1d839700b12a79ffc484d6012e39fc
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-09-10 11:39:52 +0530

    Integreate search page with API

--- app/search/page.js
+++ app/search/page.js
@@ -1,15 +1,31 @@
 "use client";
 
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import SearchBar from "@/components/SearchBar";
 import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 import Header from "@/components/Header";
+import search from "@/apis/user/search";
+import SessionTokenContext from "@/contexts/sessionToken/SessionTokenContext";
 
 export default function Search() {
+  const [users, setUsers] = useState(null);
   const searchParams = useSearchParams();
   const query = searchParams.get("query");
+  const { getSessionToken } = useContext(SessionTokenContext);
+
+  const searchUser = async (query) => {
+    const res = await search(query, getSessionToken());
+
+    if (res?.success) {
+      setUsers(res.results);
+    }
+  };
+
+  useEffect(() => {
+    searchUser(query);
+  }, [query]);
 
   return (
     <div className="w-full px-4 md:px-10 bg-white dark:bg-[#161616]">
@@ -21,29 +37,38 @@ export default function Search() {
         </h1>
         <hr className="pb-5 mb-5 mt-2" />
       </div>
-      {query ? (
+      {query && users ? (
         <div className="min-h-[calc(100vh-249px)]">
-          <Link
-            href="/profile/1"
-            className="flex items-center gap-x-4 w-full border-y px-2.5 py-3 cursor-pointer text-black dark:text-white bg-white dark:bg-transparent hover:bg-[#dfdbdb] dark:hover:bg-[#555353]">
-            <Image
-              alt="profile-pic"
-              src="/images/user.svg"
-              height={48}
-              width={48}
-              className="rounded-full w-12 h-12"
-            />
-            <div className="w-full">
-              <h2 className="font-signika text-sm line-clamp-1">John Cena</h2>
-              <p className="font-poppins text-xs line-clamp-1 break-all max-w-full">
-                <span className="mr-4">offical_johncena</span>
-                <span className="relative before:absolute before:top-1.5 before:-left-2.5 before:h-1 before:w-1 before:bg-[#555353] dark:before:bg-white before:rounded-full">
-                  An American professional wrestler, actor, and former rapper
-                  currently signed to WWE.
-                </span>
-              </p>
-            </div>
-          </Link>
+          {users?.map((user, index) => (
+            <Link
+              key={index}
+              href={`/profile/${user?._id}`}
+              className="flex items-center gap-x-4 w-full border-y px-2.5 py-3 cursor-pointer text-black dark:text-white bg-white dark:bg-transparent hover:bg-[#dfdbdb] dark:hover:bg-[#555353]">
+              <Image
+                alt="profile-pic"
+                src={user?.profilePicture || "/images/user.svg"}
+                height={48}
+                width={48}
+                className="rounded-full w-12 h-12"
+              />
+              <div className="w-full">
+                <h2 className="font-signika text-sm line-clamp-1">
+                  {user?.name}
+                </h2>
+                <p className="font-poppins text-xs line-clamp-1 break-all max-w-full">
+                  <span className="mr-4">
+                    <span className="text-[10px]">@</span>
+                    {user?.username}
+                  </span>
+                  {user.bio && (
+                    <span className="relative before:absolute before:top-1.5 before:-left-2.5 before:h-1 before:w-1 before:bg-[#555353] dark:before:bg-white before:rounded-full">
+                      {user.bio}
+                    </span>
+                  )}
+                </p>
+              </div>
+            </Link>
+          ))}
         </div>
       ) : (
         <div className="h-[calc(100vh-249px)] md:h-[calc(100vh-289px)] flex items-center text-black dark:text-white justify-center font-firasans text-lg">
--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,9 +1,33 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
+import { useRouter, useSearchParams } from "next/navigation";
+import queryString from "@/utils/queryString";
 
 export default function SearchBar() {
+  const [input, setInput] = useState("");
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const query = searchParams.get("query");
+
+  const handleForm = (event) => {
+    event.preventDefault();
+    router.replace(
+      queryString({
+        query: input,
+      })
+    );
+  };
+
+  useEffect(() => {
+    setInput(query);
+  }, []);
+
   return (
-    <form className="flex gap-3 bg-[#f5f3f3] dark:bg-[#2B2B2B] p-3.5 rounded-lg">
+    <form
+      className="flex gap-3 bg-[#f5f3f3] dark:bg-[#2B2B2B] p-3.5 rounded-lg"
+      onSubmit={handleForm}>
       <Image
         alt=""
         height={20}
@@ -15,6 +39,8 @@ export default function SearchBar() {
         type="text"
         placeholder="Search..."
         className="bg-transparent w-full outline-none border-none font-poppins text-sm placeholder:text-black dark:placeholder:text-white text-black dark:text-white"
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
       />
     </form>
   );
